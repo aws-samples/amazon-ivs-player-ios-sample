4c3144000300fd2208c4a0febc14a6e6a2e72cfb
vp-auto <SRVC-VideoPlayback-Automation@justin.tv>
2026-07-17 20:45:30 +0000

    Update AmazonIVSPlayer to version 1.54.1

--- AmazonIVSPlayer/Package.swift
+++ AmazonIVSPlayer/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonIVSPlayer",
-            url: "https://player.live-video.net/1.54.0/AmazonIVSPlayer.xcframework.zip",
-            checksum: "77a9cac0470037166adbc80290db5cf038b26835c31b9e0ddd2b6d810ac873c3"
+            url: "https://player.live-video.net/1.54.1/AmazonIVSPlayer.xcframework.zip",
+            checksum: "6572968f41e31b4a52e5d970be39e366924eaf586ee8a89cf9709b86fc93510d"
         ),
     ]
 )
